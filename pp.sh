779a9f2e9efccc9d04aba8b1555e05bd006c8f28
Linus Wallgren <linus.wallgren@gmail.com>
2016-05-28 23:31:15 +0200

    Add POSIX sh compatible version of script

---
+++ pp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# this is a POSIX sh compliant version of the script
+
+pp() {
+	# remember output, prevent different clipboard contents
+	# after choosing to execute
+	script=$(xclip -o)
+	echo "$script" | cat -A
+	printf "\nExecute? (y/n): "
+	read execute
+	# only execute when 'y' was answered
+	# all other input is ignored
+	if [ "$execute" = "y" ]; then
+		eval "$script"
+	fi
+}
