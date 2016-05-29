18307cd147908a691191da56a1c97694515e6c01
Aaron Mousavi <mousavi.aaron@gmail.com>
2016-05-29 12:30:48 +0200

    [#3] Display newlines + test changed

--- pp-compatibility.sh
+++ pp-compatibility.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+# this is a POSIX sh compliant version of the script
+
 pp() {
 	# remember output, prevent different clipboard contents
 	# after choosing to execute
 	script=$(xclip -o)
-	cat -A <<< "$script"
+	echo "$script" | cat -A
 	printf "\nExecute? (y/n): "
 	read execute
 	# only execute when 'y' was answered
 	# all other input is ignored
-	if [[ "y" == "$execute" ]]; then
+	if [ "y" = "$execute" ]; then
 		eval "$script"
 	fi
 }
